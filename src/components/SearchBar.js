6c3177b79513f26681242b13f842245807cdd98e
Rafael Lopez <lpz.raf@gmail.com>
2019-10-07 23:44:10 -0400

    It will add initial styling to the SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,9 +5,12 @@ import React, { Component } from 'react'
 export default class SearchBar extends Component {
     render() {
         return (
-            <div>
-                <form>
-                    <input type="text" />   
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" />  
+                    </div> 
                 </form>
             </div>
         )

f9ae474da44fe0d97a784d7a8311e07ec840da04
Rafael Lopez <lpz.raf@gmail.com>
2019-10-08 22:24:01 -0400

    It will add the form submit functions

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,13 +3,27 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+    
+  state = {
+      turn: ''
+  }
+
+  onFormSubmit = e => {
+      e.preventDefault();
+      console.log(this.state.turn)
+  }
+    
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />  
+                        <input 
+                            type="text" 
+                            value={this.state.turn} 
+                            onChange={(e) => this.setState({turn: e.target.value})} 
+                            />  
                     </div> 
                 </form>
             </div>
